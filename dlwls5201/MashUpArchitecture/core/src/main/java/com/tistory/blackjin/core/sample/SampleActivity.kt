af2aa9dd6e2251820a3d3a9bf7e9c1903203dc96
leejinseong <dlwls5201@naver.com>
2020-04-15 12:33:30 +0900

    코어 기능 추가

---
+++ dlwls5201/MashUpArchitecture/core/src/main/java/com/tistory/blackjin/core/sample/SampleActivity.kt
@@ -0,0 +1,5 @@
+import com.tistory.blackjin.core.R
+import com.tistory.blackjin.core.base.BaseActivity
+import com.tistory.blackjin.core.databinding.ActivitySampleBinding
+
+class SampleActivity : BaseActivity<ActivitySampleBinding>(R.layout.activity_sample)
